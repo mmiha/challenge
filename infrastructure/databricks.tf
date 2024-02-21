f8a1426880a2eb09a9a8dfff462b0bbc0073cbaf
Mihael Mihalkov <mmihalkov@outlook.com>
2024-02-22 00:21:05 +0200

    Set job.ipynb as a scheduled workflow

--- infrastructure/databricks.tf
+++ infrastructure/databricks.tf
@@ -20,6 +20,55 @@ resource "databricks_secret" "publishing_api" {
   scope        = databricks_secret_scope.challenge-storage-secret-scope.id
 }
 
-resource "databricks_repo" "challenge-repo" {
-  url = "https://github.com/mmiha/challenge.git"
+
+data "databricks_spark_version" "latest" {
+  gpu = false
+}
+
+resource "databricks_job" "challenge-job" {
+ name = "challenge-job"
+ max_concurrent_runs = 1
+
+ # job schedule
+ schedule {
+   quartz_cron_expression = "0 0 12 ? * MON-FRI"
+   timezone_id = "UTC"
+  }
+
+ # reference to git repo. Add the git credential separately
+ # through a databricks_git_credential resource
+ git_source {
+   url      = "https://github.com/mmiha/challenge.git"
+   provider = "gitHub"
+   branch   = "main"
+ }
+
+ job_cluster {
+    new_cluster {
+      custom_tags = {
+        "ResourceClass" = "SingleNode"
+      }
+
+      num_workers   = 0
+      spark_version = data.databricks_spark_version.latest.id
+      node_type_id  = "Standard_F4"
+      spark_conf                   = {
+        "spark.databricks.cluster.profile" = "singleNode" 
+        "spark.master"                     = "local[*, 4]"
+      }
+
+    }
+   job_cluster_key = "challenge_job_cluster"
+ }
+
+ task {
+   task_key = "run_challenge_job" 
+
+   notebook_task {
+     notebook_path = "src/job"
+   }
+
+   job_cluster_key = "challenge_job_cluster" 
+
+ }
 }
